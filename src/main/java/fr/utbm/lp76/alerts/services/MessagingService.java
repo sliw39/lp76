d86812afb63784e07dc3bff42e73eba420657fae
xNufarx <nufar.poubelle@gmail.com>
2013-10-25 17:08:05 +0200

    Début d'implémentation du MessagingService

--- src/main/java/fr/utbm/lp76/alerts/services/MessagingService.java
+++ src/main/java/fr/utbm/lp76/alerts/services/MessagingService.java
@@ -1,5 +1,12 @@
 package fr.utbm.lp76.alerts.services;
 
-public interface MessagingService {
+import fr.utbm.lp76.alerts.model.AlertHis;
 
+public interface MessagingService {
+	
+	/**
+	 * sendAlert is used to send a new message to the ActivMQ server. It takes an AlertHis object with will be use to fill the new message.
+	 * @param ah The alert history to publish to the ActivMQ server. If it's null the method will generate an JavaNullPointerException.
+	 */
+	public void sendAlert(AlertHis ah);
 }
