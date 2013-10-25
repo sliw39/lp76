ea5627cb5d3c303177c3ae5c5ac2b6a545810b66
sliw39 <charlie.wils@laposte.net>
2013-10-25 15:21:05 +0200

    area

--- src/main/java/model/Area.java
+++ src/main/java/model/Area.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Area {
-	private String test;
+	private String label;
+	private String road;
+	
+	
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getRoad() {
+		return road;
+	}
+	public void setRoad(String road) {
+		this.road = road;
+	}
 }
