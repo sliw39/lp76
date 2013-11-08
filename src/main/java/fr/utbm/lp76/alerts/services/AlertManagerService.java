c73412f76a45a40a0956899da3596c2777f34654
xNufarx <nufar.poubelle@gmail.com>
2013-11-08 16:40:08 +0100

    Restauration version locale

--- src/main/java/fr/utbm/lp76/alerts/services/AlertManagerService.java
+++ src/main/java/fr/utbm/lp76/alerts/services/AlertManagerService.java
@@ -1,14 +1,16 @@
 package fr.utbm.lp76.alerts.services;
 
 import fr.utbm.lp76.alerts.model.Alert;
-import fr.utbm.lp76.alerts.model.Sensor;
+import fr.utbm.lp76.alerts.model.Trigger;
 
 public interface AlertManagerService {
 	
 	/**
-	 * createAlert is use for create a new alert. It takes a parameter "al" and a paramater "se"  
+	 * createAlert is use for create a new alert. It takes a parameter "al" and a paramater "tg"  
 	 * @param al
 	 * @param se
 	 */
-	public void createAlert(Alert al, Sensor se);
+	public void createAlert(Alert al, Trigger tg);
+	
+	public void stopAlert(Alert al);
 }
